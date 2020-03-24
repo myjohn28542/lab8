b096cb8c38e8d33e7a2dd64ac4e6df7715f39d1c
myjohn28542 <john2854@gmail.com>
2020-03-24 21:58:59 +0700

    Updates

---
+++ precache-manifest.93f6f073ce3d8541966509e6a2527174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40677b1e7ef932cd1a25624bdc8112e",
+    "url": "/lab8/index.html"
+  },
+  {
+    "revision": "dd494508f8564b22f76c",
+    "url": "/lab8/static/css/main.4851e9fc.chunk.css"
+  },
+  {
+    "revision": "1accadeae75dc65f9865",
+    "url": "/lab8/static/js/2.48b2c16e.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/lab8/static/js/2.48b2c16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd494508f8564b22f76c",
+    "url": "/lab8/static/js/main.df1f83a2.chunk.js"
+  },
+  {
+    "revision": "02d5367f5cf3fe1aeb45",
+    "url": "/lab8/static/js/runtime-main.89521340.js"
+  }
+]);
