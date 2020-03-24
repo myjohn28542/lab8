ce0719cb82ba9c2795e80a3aa509161e54723670
myjohn28542 <john2854@gmail.com>
2020-03-24 22:07:31 +0700

    oioi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
             <h5 className="card-title">Developer information</h5>
             <p>suphachai mawong (610610618)</p>
             <p>This app use Google Firebase as backend.</p>
-            <Link to="/">
+            <Link to="/lab8/">
               <h3>
                 <button type="button" className="btn btn-primary">Home</button>
               </h3>
